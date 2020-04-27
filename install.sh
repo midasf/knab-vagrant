efbfedde7d2bf69568dbad6f0036f7ea1a5c0c69
midasfuld <midas.fuld@knab.nl>
2020-04-27 21:10:50 +0200

    first commit

---
+++ install.sh
@@ -0,0 +1,11 @@
+      mkdir -p /home/linuxbrew/    
+    git clone https://github.com/Homebrew/brew /home/linuxbrew/.linuxbrew/Homebrew 
+    #git clone https://github.com/Homebrew/brew Homebrew
+    mkdir -p /home/linuxbrew/.linuxbrew/bin
+    # ln -s /home/linuxbrew/Homebrew/bin/brew /home/linuxbrew/.linuxbrew/bin/ 
+    ln -s /home/linuxbrew/.linuxbrew/Homebrew/bin/brew /home/linuxbrew/.linuxbrew/bin/ 
+  /home/linuxbrew/.linuxbrew/bin/brew install gcc
+  /home/linuxbrew/.linuxbrew/bin/brew install python
+  /home/linuxbrew/.linuxbrew/bin/brew install aws-vault
+  /home/linuxbrew/.linuxbrew/bin/brew install terraform
+  echo export PATH="$PATH:/home/linuxbrew/.linuxbrew/bin" >> ~/.profile
